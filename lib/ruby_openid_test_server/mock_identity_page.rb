e92ce82dba58423eaa610beb8ca1a3a4b76c3983
Roman Gonzalez <romanandreg@gmail.com>
2009-02-19 23:38:06 -0800

    Adding a list thing needed on the mock identity page app in order to make it work as expected

--- lib/ruby_openid_test_server/mock_identity_page.rb
+++ lib/ruby_openid_test_server/mock_identity_page.rb
@@ -9,12 +9,14 @@ class RubyOpenIdTestServer::MockIdentityPage
   end
   
   def call(env)
+    request = Rack::Request.new(env)
+    flag = request.params['openid.success'] == 'true' ? '?openid.success=true' : ''
     Rack::Response.new do |response|
       response.write <<-HERE
 <html>
   <head>
-  <link rel="openid.server" href="http://localhost:1123/" />
-  <link rel="openid2.provider" href="http://localhost:1123" />
+  <link rel="openid.server" href="http://localhost:1123/#{flag}" />
+  <link rel="openid2.provider" href="http://localhost:1123/#{flag}" />
   </head>
   <body>
     <h1>This is #{@config['identity']} identity page</h1>
