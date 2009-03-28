b4c2ad398363c4b2db33dbb9dc9ae13017bbf259
Roman Gonzalez <romanandreg@gmail.com>
2009-03-28 10:16:32 -0700

    Making the test helper less smarter

--- lib/rots/test_helper.rb
+++ lib/rots/test_helper.rb
@@ -9,14 +9,7 @@ module Rots::TestHelper
     openid_response_uri = URI(openid_response['Location'])
     openid_response_qs = Rack::Utils.parse_query(openid_response_uri.query)
     
-    # invoking the OpenID consumer with the options of the response of the ServerApp
-    if defined?(Webrat)
-      visit(openid_response_uri.to_s)
-    else
-      # NOTE: Works on Rails and Merb frameworks via duck typing
-      # later we will check on Sinatra
-      get(openid_response_uri.path, openid_response_qs)
-    end
+    {:url => openid_response_uri.to_s, :query_params => openid_response_qs}
   end
   
 end
